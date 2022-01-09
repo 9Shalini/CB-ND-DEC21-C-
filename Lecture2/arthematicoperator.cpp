b28012b2795710a8ab719a11f1af90ea9ff241da
Mosina916 <mosinaashraf43@gmail.com>
2022-01-09 17:00:07 +0530

    Basics

---
+++ Lecture2/arthematicoperator.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	// unary opertors
+	// -5 +7
+
+
+	// arthematic operator
+	int s=10+5;
+
+	cout<<s<<endl;//15
+	int d=80-30;//subtraction
+
+	cout<<d<<endl;//50
+
+	int m=7*5;
+	cout<<m<<endl;//35
+
+
+	int div=60/6;
+	cout<<div<<endl;//10
+
+
+	// int r=60%6;//if 60 is divide by 6 so it gives the remainder
+	// cout<<r<<endl;//0
+
+
+	int r=(60%6)+9;//if 60 is divide by 6 so it gives the remainder
+	cout<<r<<endl;//0
+
+
+
+	// = assignment pehle apne rhs pura evavlaute karta hai then lhs mai daal deta hai
+	
+
+
+
+
+
+	return 0;
+}
---
+++ Lecture2/conditionals.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// int rollno=50;
+	// // comaprison ==-->relational operator
+
+	// // syntax
+	// // if(condition){
+	// // 	// agar conition true
+	// // }
+	// // agar conition false
+
+	// // single if 
+	// // == -->o/p true  false
+	// if
+	// if(rollno==50){//true false
+	// 	cout<<"i will gift you a car"<<endl;
+
+	// }
+
+
+
+	// int rollno=60;
+	// // agar
+	// // if else
+	// if(rollno==50){//true false
+	// 	cout<<"i will gift you a car"<<endl;
+
+	// }
+	// // else nahi tou
+	// else{
+	// 	cout<<"i will gift you nothing"<<endl;
+
+	// }
+
+
+	// // if elseif .... else
+
+	// int rollno=75;
+	// if(rollno==50){
+	// 	cout<<"i will gift you a car"<<endl;
+
+	// }
+	// else if(rollno==60){
+	// 	cout<<"i will gift you a phone"<<endl;
+
+	// }
+	// else if(rollno==70){
+	// 	cout<<"i will gift you an earphone"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"i will gift you nothing"<<endl;
+
+	// }
+
+
+
+	// // if if 
+	// int age=22;
+
+	// if(age>=20){
+	// 	cout<<"i can drive a car"<<endl;
+
+	// }
+	// // else{
+	// // 	cout<<"i can go to a picnic"<<endl;
+
+	// // }
+	// if(age<=15){
+	// 	cout<<"i can go to a picnic"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"i can't go to a picnic"<<endl;
+
+	// }
+
+	// if(age!=23){
+	// 	cout<<"hello"<<endl;
+
+	// }
+
+// non zero value-->true
+	if(-9){
+		cout<<"coding blcoks"<<endl;
+	}
+	else{
+		cout<<"hello world"<<endl;
+
+	}
+
+	if(93){
+		cout<<"coding blcoks"<<endl;
+	}
+	else{
+		cout<<"hello world"<<endl;
+
+	}
+
+	if(0){
+		cout<<"coding blcoks"<<endl;
+	}
+	else{
+		cout<<"hello world"<<endl;
+
+	}
+
+
+
+
+
+
+	return 0;
+}
