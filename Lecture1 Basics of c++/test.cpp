4cb3f77bd18a679cbbc3707cb996cb214edd028b
Mosina916 <mosinaashraf43@gmail.com>
2022-01-09 10:17:49 +0530

    Basics of c++

---
+++ Lecture1 Basics of c++/test.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main(){//start of program-->sing line comment 
+	// making of buckets
+	// store hoga garbage value
+
+	// int p;
+	// int r;
+	// int t;
+	// decalre
+	int p,r,t;
+	// overide garbage values 
+	// assign
+	// user define 
+	// p=10;
+	// r=70;
+	// t=40;
+
+	cin>>p>>r>>t;//1 10 1-->10/100-->0.1
+
+// initilization 
+	// int si=(p*r*t)/100;//aasignment operator ka rhs evavluate and then assign hota hai
+	// float si=(p*r*t)/100.0;//int/int-->int //float/int or int/float or float/float-->float
+	// float si=(p*r*t*1.0)/100;
+	// implicit type casting or type conversion
+	float si=(p*r*t*1.0)/100.0;
+	cout<<si<<endl;
+
+	return 0;//compiler -->terminate succesuffuly
+
+	// print -->cout-->console o/p <<
+
+	// cout<<p<<'\n';//'\n'-->nect line
+	// cout<<r<<'\n';
+	// cout<<t<<'\n';
+
+	// cout<<p<<'\n'<<r<<endl<<t<<'\n';
+
+
+	// // string print
+
+	// cout<<"hello world"<<'\n';//endl-->new line
+
+
+	// // chacter print
+	// cout<<'G'<<endl;
+
+
+	// // number 
+	// cout<<4675<<endl;
+
+
+	// character variable
+
+	// char ch='u';
+	// char ch3='o';
+	// char ch1;
+	// ch1='p';
+
+	char ch,ch1,cha1,ch2,c;
+	ch='r';
+	ch1='p';
+
+
+	cout<<ch<<'\n'<<ch1<<'\n'<<cha1<<endl;
+
+
+	float f=42.786;
+	cout<<f<<endl;
+
+
+
+
+
+
+	// return 0;//compiler -->terminate succesuffuly
+}
